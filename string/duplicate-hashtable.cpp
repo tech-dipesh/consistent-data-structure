966eb99015fa7c6c9f8ea05b42b282230f5d2b1c
Dipesh <diepshgautambusiness@gmail.com>
2025-04-30 23:34:20 +0530

    add teh hashtabel duplicate code

--- string/duplicate-hashtable.cpp
+++ string/duplicate-hashtable.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 using namespace std;
 
-hastable main(char *name[]){
-  int H[26];
+bool hastable(string name){
+  int H[26]={};
   for(int i=0;name[i]!='\0';i++){
     // if(H[])
-    H[A[i]-97]+=1;
+    H[name[i]-97]+=1;
   }
   for(int i=0;i<26;i++){
-    if(h[i]>1){
+    if(H[i]>1){
       // printf("%d", i+97);
-      cout<< i+97;
+      // cout<< i+97;
+      cout<< "Duplicate character:"<<char(i+'a')<<endl;
+      return true;
       // return H[i];
-      cout << i+97;
-      return 0;j
+      // cout << i+97;
     }
   }
-  return -1;
+  return false;
 }
 
 
 int main(){
-  char *name[];
+  // string name= "DuplicateValue";
+  string name;
+  cin>>name;
 
-  name[]="DuplicateValue"
-    if(hastable){
+    if(hastable(name)){
       // printf("Yes the value is string");
       cout<<"Yes the value have the duplicate";
     }
-    {
+    else{
       cout<< "No the value is not having a duplicate";
     }
 return 0;
