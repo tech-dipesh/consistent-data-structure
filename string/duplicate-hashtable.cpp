4e572df0ab1e2c31ba8a9da85237fd686fad019c
Dipesh <diepshgautambusiness@gmail.com>
2025-04-30 22:52:23 +0530

    started a blank commit

---
+++ string/duplicate-hashtable.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+hastable main(char *name[]){
+  int H[26];
+  for(int i=0;name[i]!='\0';i++){
+    // if(H[])
+    H[A[i]-97]+=1;
+  }
+  for(int i=0;i<26;i++){
+    if(h[i]>1){
+      // printf("%d", i+97);
+      cout<< i+97;
+      // return H[i];
+      cout << i+97;
+      return 0;j
+    }
+  }
+  return -1;
+}
+
+
+int main(){
+  char *name[];
+
+  name[]="DuplicateValue"
+    if(hastable){
+      // printf("Yes the value is string");
+      cout<<"Yes the value have the duplicate";
+    }
+    {
+      cout<< "No the value is not having a duplicate";
+    }
+return 0;
+}
