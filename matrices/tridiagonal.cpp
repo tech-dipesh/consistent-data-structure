72ad79631037d8d33faa4903b6dde50e7e9468c8
Dipesh <dipeshsharmagautam@outlook.com>
2025-05-12 10:22:58 +0530

    start the tridiagonal element

---
+++ matrices/tridiagonal.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+cout<<"hello this is the tridiagonal element of the trdiagonal."
+int n, x;
+int *A=new size[n];
+for(int i=)
+return 0;
+}
