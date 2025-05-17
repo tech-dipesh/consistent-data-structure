6a0f88862a27b32ab7d6eb965dc0bfbaf326ede2
Dipesh <dipeshsharmagautam@outlook.com>
2025-05-17 11:47:58 +0530

    add the band fle

--- matrices/band.cpp
+++ matrices/band.cpp
@@ -1,2 +1,9 @@
 // This the band  am talking abut.
 // which it needs to band the code, with the better approach.
+#include <iostream>
+using namespace std;
+int main(){
+    
+cout<<"band logic i could do t";
+return 0;
+}
