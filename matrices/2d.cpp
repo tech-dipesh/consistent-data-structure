6df61f848680f6c1dca0f5b5c23cadffa804ac62
Dipesh <dipeshsharmagautam@outlook.com>
2025-05-23 00:42:00 +0530

    add the file

--- matrices/2d.cpp
+++ matrices/2d.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
+int output(){
+  return 0;
+}
+
 int main(){
-    
+  output();
 cout<<"this is the just sample";
+cout<<"this is the output ii'm trying to setup"
 return 0;
 }
