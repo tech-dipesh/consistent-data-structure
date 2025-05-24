fa80e8dc626f75b29db82ccff5432ef7aa04322d
Dipesh <dipeshsharmagautam@outlook.com>
2025-05-24 10:41:28 +0530

    update just for the shake of the consistency

--- matrices/2d.cpp
+++ matrices/2d.cpp
@@ -7,6 +7,7 @@ int output(){
 int main(){
   output();
 cout<<"this is the just sample";
+cout<<"2d array with the value of the data structure and algorithm."
 cout<<"this is the output ii'm trying to setup"
 return 0;
 }
