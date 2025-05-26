5b5c73a78f627e990f7e381ac7bbae6e3b30b33d
Dipesh <dipeshsharmagautam@outlook.com>
2025-05-27 00:11:30 +0530

    update the sparse

--- matrices/sparse.cpp
+++ matrices/sparse.cpp
@@ -5,5 +5,8 @@ using namespace std;
 int main(){
    cout<<"sparse matrix is combination of the two triangular matrix combination." 
 cout<<"wow what a combination matrix.";
+cout<<"it will have the most of teh elements as a 0";
+cout<<"we can find out if a matrix is sparse using the formula:";
+cout<<"If (Number of zero elements) > (Total elements) / 2, then it is a sparse matrix.";
 return 0;
 }
