aa8097c3c9faf1b3f19f63af41b7510ca5b6bd03
Dipesh <dipeshsharmagautam@outlook.com>
2025-05-28 11:01:47 +0530

    update the sparse

--- matrices/sparse.cpp
+++ matrices/sparse.cpp
@@ -8,5 +8,19 @@ cout<<"wow what a combination matrix.";
 cout<<"it will have the most of teh elements as a 0";
 cout<<"we can find out if a matrix is sparse using the formula:";
 cout<<"If (Number of zero elements) > (Total elements) / 2, then it is a sparse matrix.";
+cout<<"during the sparase metrix we can just cheeck on the 2d form."
+cout<<"so we need to use the both  i and j with iniit like this:"
+
+int n, rows, colums;
+
+cin>>n;
+cin>>rows;
+cin>>colums;
+int matrix[n][n];
+for(int i=0;i<n;i++){
+   for(int j=0;j<n;j++){
+      matrix[i][j];
+   }
+}
 return 0;
 }
