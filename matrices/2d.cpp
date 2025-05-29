bb658e1ab4fa923fbaedfd27f047365108ff08b2
Dipesh <dipeshsharmagautam@outlook.com>
2025-05-29 13:48:39 +0530

    change 2d

--- matrices/2d.cpp
+++ matrices/2d.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
-int output(){
+int output(int row, int col, int matrix){
+  for(int i=0;i<row;i++){
+    for(int j=0;j<row;j++){
+      
+    }
+  }
   return 0;
 }
 
 int main(){
-  output();
+  int row, col, matrix;
+  cin>>row;
+  cin>>col;
+  cin>>matrix;
+  output(row, col, matrix);
 cout<<"this is the just sample";
 cout<<"2d array with the value of the data structure and algorithm."
 cout<<"this is the output ii'm trying to setup"
