9291dcddf1492224a3eb17044fbba3fd3e8c255e
Dipesh <dipeshsharmagautam@outlook.com>
2025-05-30 09:31:28 +0530

    update the tridigonal

--- matrices/tridiagonal.cpp
+++ matrices/tridiagonal.cpp
@@ -5,6 +5,18 @@ int main(){
 cout<<"hello this is the tridiagonal element of the trdiagonal."
 int n, x;
 int *A=new size[n];
-for(int i=)
+for(int i=0;i<n;i++){
+  cin>>A;
+  
+}
+int row, cols, output, num;
+for(int i=0;i<n;i++){
+  for(int j=0;j<n;j++){
+    row[i][j];
+    cols[j][i];
+    output=num[row][cols]
+  }
+}
+cout<<output;
 return 0;
 }
