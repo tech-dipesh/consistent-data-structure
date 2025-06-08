47d2e623bc1dd837be27e524ece16c05875dd7e3
Dipesh <dipeshsharmagautam@outlook.com>
2025-06-08 15:57:08 +0545

    add a ll file

---
+++ lnkedilist/create-jenny-help.cpp
@@ -0,0 +1,50 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+struct  node{
+  int data;
+  node* next;
+};
+
+int main(){
+// node*head, *newnode;
+node *head=nullptr, *temp=nullptr;
+int choice=1;
+// int head=0, choice;
+while(choice==1){
+node* newnode=new node;
+cout<<"Enter a data";
+cin>>newnode->data;
+newnode->next=nullptr;
+
+if(head==nullptr)
+  head=temp=newnode;
+
+else{
+  temp->next=newnode;
+  temp=newnode;
+}
+cout<<"Do you want to continue (0, 1)?";
+cin>>choice;
+
+}
+
+// This is for the display a list
+temp=head;
+cout<<"linked list ";
+while(temp!=nullptr){
+  cout<<temp->data<<endl;
+  temp=temp->next;
+  // count++;
+}
+
+
+// this is for my undersand we need to clean a mmmeory:
+while(head!=nullptr){
+  node* toDelete=head;
+  head=head->next;
+  delete toDelete;
+}
+return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,38 +1,10 @@
-#include <bits/stdc++.h>
-struct  node{
-  int data;
-  node* next;
-}
-
+#include <iostream>
+using namespace std;
 int main(){
-node*head, *newnode;
-node *head=nullptr, *temp=nullptr;
-int choice=0;
-// int head=0, choice;
-while(choice){
-node* newnode=new node;
-cout<<"Enter a data";
-cin>>newnode->data;
-newnode->next=nullptr;
-
-if(head==nullptr)
-  head=temp=newnode;
-
-else{
-  temp->next=newnode;
-  temp=newnode;
-}
-cout<<"Do you want to continue (0, 1)?";
-cin>>choice;
-
-}
-temp=head;
-cout<<"linked list";
-while(temp!=nullptr){
-  cout<<temp->data;
-  temp=temp->next;
-  // count++;
-}
-
+  //  int x=4;
+  int arry[2]={9, 19};
+  cout<<1[arry];
+  //  int * px=&x;
+  //  int y=*px; 
 return 0;
 }
