cbfa7c25f5f60ad030e80edccfdd2ec6458b9e09
Dipesh <dipeshsharmagautam@outlook.com>
2025-06-12 22:54:40 +0545

    update a count code

---
+++ lnkedilist/count.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+struct Node{
+  int n;
+  Node* next;
+}
+int LinkedList(int num){
+  if(num==0) return 0;
+  return LinkedList(num-1)-num;
+}
+
+int main(){
+    
+cout<<LinkedList(45);
+return 0;
+}
