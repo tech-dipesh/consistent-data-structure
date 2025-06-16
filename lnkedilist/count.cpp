930ec5df1dad09e315dc38be61dcfea75bd664bf
Dipesh <dipeshsharmagautam@outlook.com>
2025-06-16 10:06:44 +0545

    update a count file

--- lnkedilist/count.cpp
+++ lnkedilist/count.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 struct Node{
   int n;
   Node* next;
 }
+
 int LinkedList(int num){
+  
   if(num==0) return 0;
   return LinkedList(num-1)-num;
 }
