49a0d9e6797a835066c77ef6e57759f1c741dc67
Dipesh <dipeshsharmagautam@outlook.com>
2025-06-29 20:05:47 +0530

    update a double linked list

--- lnkedilist/doubly-linked-list.cpp
+++ lnkedilist/doubly-linked-list.cpp
@@ -4,7 +4,14 @@ struct Linked{
   int num;
   Node* Linked name;
 }
+class Show{
+  public:
+  int DoubleLinkedList(int num){
+    int value;
+    Node* show;
+  }
+}
 int main(){
-    
+  DoubleLinkedList(29, 20);    
 return 0;
 }
