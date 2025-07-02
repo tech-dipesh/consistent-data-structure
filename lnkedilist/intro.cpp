227fe75d73adabf706acf95b7e2a892b47f2ecdc
Dipesh <dipeshsharmagautam@outlook.com>
2025-07-02 08:01:56 +0530

    update a intro page

--- lnkedilist/intro.cpp
+++ lnkedilist/intro.cpp
@@ -1,13 +1,26 @@
 // #include <iostream>
 #include <stdlib.c++.h>
+// #include <vector>
+// #include <algorithm>
 using namespace std;
-#include <vector>
-#include <algorithm>
 struct Node {
     int data;
     Node* next;
 };
 
+// class defination on the double linked list
+class Show{
+  public:
+  int DoubleLinkedList(int num){
+    int value;
+    Node* show;
+  }
+  int Insertion(Node* show){
+    Node* temp=show;
+    Node* front=show->next;
+    Node* bacck=temp->back;
+  }
+}
 int main() {
     Node* myNode = new Node{10, nullptr}; 
     // above value stored on the value and address beloe
