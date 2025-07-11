b366a864a44f984b94de5eba3a7275ae31f45b36
Dipesh <dipeshsharmagautam@outlook.com>
2025-07-11 10:27:59 +0530

    add a search file and update a count file

--- lnkedilist/count.cpp
+++ lnkedilist/count.cpp
@@ -1,19 +1,56 @@
 #include <iostream>
-// #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
-struct Node{
-  int n;
-  Node* next;
-}
-
-int LinkedList(int num){
-  
-  if(num==0) return 0;
-  return LinkedList(num-1)-num;
-}
-
-int main(){
+// singly liinked list:
+ struct ListNode {
+     int val;
+    ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+class Solution {
+public:
+    int countNodes(ListNode* head) {
+      int count=0;
+      while(head){
+        count++;
+        head->next;
+      }
+      return count;
+    }
     
-cout<<LinkedList(45);
-return 0;
+    // create a linked list from a vector
+    ListNode* createLinkedList(const vector<int>& values) {
+        if (values.empty()) return nullptr;
+        
+        ListNode* head = new ListNode(values[0]);
+        ListNode* current = head;
+        
+        for (size_t i = 1; i < values.size(); ++i) {
+            current->next = new ListNode(values[i]);
+            current = current->next;
+        }
+        
+        return head;
+    }
+    
+    // for free a memory which is imp imo
+    // it will remove every items from the list
+    void deleteLinkedList(ListNode* head) {
+        while (head != nullptr) {
+            ListNode* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+
+  int main() {
+  ListNode* head = solution.createLinkedList({1, 2, 3, 4, 5});
+ int nodeCount = solution.countNodes(head);
+ cout << "Number of nodes: "<< nodeCount;
+  solution.deleteLinkedList(head);
+  return 0;
 }
---
+++ lnkedilist/search.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+// #include <stdio.h>
+// #include <stdlib.h>
+using namespace std;
+struct Node
+{
+ int data;
+ struct Node *next;
+}*first=NULL;
+
+void create(int A[],int n){
+ int i;
+ struct Node *t,*last;
+first =new Node;
+ first->data=A[0];
+ first->next=NULL;
+ last=first;
+
+ for(i=1;i<n;i++){
+  t=new Node();
+ t->data=A[i];
+ t->next=NULL;
+ last->next=t;
+ last=t;
+ }
+}
+ Node * LSearch(struct Node *p,int key){
+ Node *q;
+ while(p!=NULL)
+ {
+ if(key==p->data)
+ {
+ q->next=p->next;
+ p->next=first;
+ first=p;
+ return p;
+ }
+ q=p;
+ p=p->next;
+ }
+ return NULL;
+
+}
+struct Node * RSearch(struct Node *p,int key)
+{
+ if(p==NULL)
+ return NULL;
+ if(key==p->data)
+ return p;
+ return RSearch(p->next,key);
+}
+int main()
+{
+ struct Node *temp;
+ int A[]={3,5,7,10,25,8,32,2};
+ create(A,8);
+ temp=Search(first,8);
+cout<<temp-data;
+ return 0;
+}
