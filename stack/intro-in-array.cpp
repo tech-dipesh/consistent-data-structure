8c31aaa13b93185fdc22fbd9f3a67790102c71cf
Dipesh <dipeshsharmagautam@outlook.com>
2025-07-17 14:21:24 +0530

    fixing more error on the intro file

--- stack/intro-in-array.cpp
+++ stack/intro-in-array.cpp
@@ -9,7 +9,7 @@ class stack{
 public:
     void Create(stack *st);
     void Display(stack *st);
-    void Push(stack *st);
+    void Push(stack *st, int x);
     // Push()
     int Pop(stack *st, int x);
   int Peek(stack *st) ;
@@ -25,10 +25,10 @@ public:
         st->top=-1;
         // st->s=new st->size;
         st->s=new int[st->size];
-
+          cout<<"successfully allocated a memory";
       }
 
-      void Display{stack *t}{
+      void Display(stack *t){
         // for(int i=top;i>=0;i--)
         for(int i=st->top;i>=0;i--)
             // cout<<st.s[i];
@@ -45,7 +45,7 @@ public:
         }
       }
 
-      void Pop(stack *st){
+      int Pop(stack *st){
         int x=-1;
         if(st->top==-1){
           cout<<"Stack underflow";
@@ -58,25 +58,27 @@ public:
 
       int Peek(stack *st, int index){
         int x=-1;
-        if(st.top-index+1<0)
-          cout<<"invalid index"<<end;
+        // if(st.top-index+1<0)
+        if(st->top-index+1<0)
+          cout<<"invalid index"<<endl;
 
-          x=st.s[st.top-index+1];
+          // x=st.s[st.top-index+1];
+          x=st->s[st->top-index+1];
 
           return x;
 
       }
 
-      int isEmpty(stack st){
+      int isEmpty(stack *st){
         if(st.top==-1)
           return 1;
           return 0;
       }
 
-      int isFull(stack st){
+      int isFull(stack *st){
         return st.top==st.size-1;
       }
-      int stackTop(stack st){
+      int stackTop(stack *st){
         if(!isEmpty(st))
           return st.s[st.top];
           return -1;
@@ -95,8 +97,8 @@ public:
   // cout<<endl<<pop(&st);
   cout<<endl<<Pop(&st);
   // cout<<endl<<peep(st, 1);
-  cout<<endl<<Peek(st, 1);
+  cout<<endl<<Peek(&st, 1);
 
-  Display(st);
+  Display(&st);
 return 0;
 }
