faadfd60e6a7bd40b269061884694f8da54fdee1
Dipesh <dipeshsharmagautam@outlook.com>
2025-07-17 09:57:01 +0530

    class on a intro of the stack

--- stack/intro.cpp
+++ stack/intro.cpp
@@ -3,7 +3,33 @@ using namespace std;
 
 class Stack{
   public:
-    
+    int size;
+    int top;
+    int *s;
+
+    public:
+      void create(Stack *st);
+
+      void create(Stack *st){
+        cout<<"Enter Size";
+        cin>>&st->size;
+        st->top=-1;
+        st->s=new st->size;
+
+      }
+
+      void Display{}{
+        for(int i=top;i>=0;i--){
+
+
+        }
+      }
+      void push(Stack *st, int x){
+        if(st->top==st->size-1){
+          cout<,"Stack Overflow";
+        }
+        else
+      }
 };
 int main(){
     
