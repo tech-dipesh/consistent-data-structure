059a6b6844c4bf455775188c48df65c14d6c2821
Dipesh <dipeshsharmagautam@outlook.com>
2025-07-17 14:09:51 +0530

    update a stack with using a class for better security

--- stack/intro.cpp
+++ stack/intro.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 using namespace std;
 
-class Stack{
-  public:
+class stack{
+  private:
     int size;
     int top;
     int *s;
-
-    void create(Stack *st);
-    void Display();
-    Push()
-    public:
-
+public:
+    void create(stack *st);
+    void Display(stack *st);
+    void push(stack *st);
+    // Push()
+    int Pop(stack *st);
+  int peek(stack *st) ;
+  int isEmpty(stack *st);
+  int isfull(stack *st); 
+  int stackTop(stack *st);
+    ~stack() {delete [] s;}
+  };
       
-      void create(Stack *st){
+      void create(stack *st){
         cout<<"Enter Size";
         cin>>&st->size;
         st->top=-1;
@@ -22,36 +28,36 @@ class Stack{
 
       }
 
-      void Display{Stack *t}{
+      void Display{stack *t}{
         for(int i=top;i>=0;i--)
             cout<<st.s[i];
               cout<<endl;
       }
-      void Push(Stack *st, int x){
+      void Push(stack *st, int x){
         if(st->top==st->size-1){
-          cout<,"Stack Overflow";
+          cout<<"stack Overflow";
         }
         else{
           st->top++;
-          st->S[st->to[]=x;]
+          st->s[st->top]=x;
         }
       }
 
-      void pop(Stack *st){
+      void pop(stack *st){
         int x=-1;
         if(st->top==-1){
-          coiut<<"Stack underflow"
+          cout<<"Stack underflow"
         }
         else{
-          x=st->tS[st->top--];
+          x=st->s[st->top--];
         }
         return x;
       }
 
-      int peek(Stack st, int index){
+      int peek(stack st, int index){
         intx=-1;
-        if(st.top-index+<0)
-          cout<,"invalid index"<<end;
+        if(st.top-index+1<0)
+          cout<<"invalid index"<<end;
 
           x=st.s[st.top-index+1];
 
@@ -73,9 +79,8 @@ class Stack{
           return st.s[st.top];
           return -1;
       }
-};
-int main(){
-    Stack st;
+      int main(){
+    stack st;
   creat(&st);
 
   push(&st, 10);
