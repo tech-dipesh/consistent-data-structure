822afe762a7392649eb78c84d3d85d5e516423a0
Dipesh <dipeshsharmagautam@outlook.com>
2025-07-21 16:05:25 +0530

    write a entire code of the intro

--- queues/intro.cpp
+++ queues/intro.cpp
@@ -9,6 +9,10 @@ class Queues{
     int rear;
     int *Q;
     public:
+    // Constructor value:
+    Queues(int sz){(sz), Q(new int[q=>size]), front(-1), rear(-1);}
+    // Destructor value:
+    ~Queues(delete [] Q);
       void Enqueue(int x);
       int Dequeue(int x);
       int isFull (int rear, int size);
@@ -16,4 +20,52 @@ class Queues{
 
 };
 
-void Queues::Enqueue(int )
+void Queues::Enqueue(Queues *Q,  x){
+  if(Q->rear==q->size-1)
+    cout<<"Queues is full";
+    else{
+      Q->rear++;
+      Q->q[q->rear]=x;
+    }
+}
+int Queues::Dequeue(Queues *q){
+  int x=-1;
+  if(q->front==q->rear)
+    cout<<"Queue is Empty";
+    else{
+      q->front++;
+      x=q->Q[q->front];
+    }
+    return x;
+}
+
+void Queues::isFull(int rear, int size){
+    if(rear==size-1) cout<<"Queue is Full";
+}
+
+void Queues::isEmpty(int front, int rear){
+    if(front==rear) cout<<"Queue is empty";
+}
+
+int main(){
+  Queues q;
+  
+  cout<<"Enter Size";
+  cin>>q->size;
+
+  
+  q.Dequeue(10);
+  q.Dequeue(25);
+  q.Dequeue(30);
+  q.Dequeue(35);
+
+  q.Enqueue();
+  q.Enqueue();
+  q.Enqueue();
+
+  
+  q.isEmpty();
+
+  q.isFull();
+  return 0;
+}
