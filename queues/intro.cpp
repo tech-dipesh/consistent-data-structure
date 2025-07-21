2a14e3dfa983cc55bb4cad5a3060f7ee7117582b
Dipesh <dipeshsharmagautam@outlook.com>
2025-07-21 16:56:52 +0530

    update a intro file

--- queues/intro.cpp
+++ queues/intro.cpp
@@ -19,38 +19,45 @@ class Queues{
     ~Queues(){delete [] Q;}
 
     // All element valus.
-   void Enqueue(Queues *Q, int x){
+  //  void Enqueue(Queues *Q, int x){
+   void Enqueue(int x){
   // if(Q->rear==q->size-1)
   if(isFull()){
     cout<<"Queues is full";
     return;
   }
-    else{
-      Q->rear++;
+  // the front should move to one step
+  if(front==-1)front=0;
+      // Q->rear++;
+      // rear++;
       // Q->q[q->rear]=x;
-      Q[rear]=x;
-}
-
+      Q[rear++]=x;
+   }
 // void Dequeue(Queues *q){
-void Dequeue(){
-  
+int Dequeue(){
+
   int x=-1;
   // if(q->front==q->rear)
   if(isEmpty())
     cout<<"Queue is Empty";
-    else{
-      q->front++;
-      x=q->Q[q->front];
-    }
-    return x;
+    // add both value.
+    int val=Q[front];
+    if(front==rear) front=rear=-1;
+    else
+      // q->front++;
+      front++;
+      // x=q->Q[q->front];
+        // x=Q[front];
+    
+    return val;
 }
 
-int isFull(int rear, int size){
+int isFull(){
     // if(rear==size-1) cout<<"Queue is Full";
     return rear==size-1;
 }
 
-int isEmpty(int front, int rear){
+int isEmpty(){
     // if(front==rear) cout<<"Queue is empty";
     return front==rear;
 }
@@ -61,7 +68,9 @@ void Display()const{
     cout<<"Queues is empty";
     return;
   }
-  for(int i=0;i<size;i++)
+  // for(int i=0;i<size;i++)
+  // first value will start from front+1 due to the, the first value will be after a the front value with two pointer apporach
+  for(int i=front+1;i<=rear;i++)
     cout<<Q[i]<<" ";
     cout<<endl;
 }
@@ -70,12 +79,12 @@ void Display()const{
 
 
 int main(){
-  Queues q;
-  
+  int size;
   cout<<"Enter Size";
-  cin>>q->size;
-
-  // Qith specified size;
+  // cin>>q->size;
+  cin>>size;
+  
+  // create the queues with the specified size
   Queues q(size);
   q.Enqueue(10);
   q.Enqueue(25);
@@ -83,7 +92,8 @@ int main(){
   q.Enqueue(35);
 
   q.Display();
- cout<<"Dequeue value is:"<<q.Dequeues<<endl;
+//  cout<<"Dequeue value is:"<<q.Dequeues<<endl;
+ cout<<"Dequeue value is:"<<q.Dequeue()<<endl;
   
  q.Display();
   return 0;
