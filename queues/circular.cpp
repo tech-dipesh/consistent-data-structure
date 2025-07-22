f483d36a1ed17d3d38fe8da9fe3ac5e7c5b06517
Dipesh <dipeshsharmagautam@outlook.com>
2025-07-22 17:24:21 +0530

    write a code with error

--- queues/circular.cpp
+++ queues/circular.cpp
@@ -16,11 +16,22 @@ class Circular{
   public:
   Circular(int sz):(sz), front(-1), rear(-1){int Q[size];}
   ~Circular(){delete [] Q;}
-  void Enqueue(){
-    
+  void Enqueue(int x){
+    if(isFull()) cout<<"Enqueue is Full";
+    else{
+      // Q[size]=;
+      rear=(rear+1)%q;
+      Q[rear]=x;
+    }
   }
-  int Dequeue(int x){
-
+  int Dequeue(){
+    int x=-1;
+    if(isEmpty()) cout<<"Dequeue is Empty";
+    else{
+      front=(front+1)%size;
+      x=Q[front];
+    }
+    return x;
   }
 
   void isFull(){
@@ -29,6 +40,14 @@ class Circular{
   void isEmpty(){
     if(front==rear)cout<<"Queue is Empty";
   }
+
+  void Display(){
+    int i=front+1;
+    for(int i=(i+1)%size;i!=(rear+1)%size;i++){
+      cout<<Q[i];
+    }
+    cout<<endl;
+  }
 };
 
 int main(){
@@ -36,6 +55,14 @@ int main(){
   cout<<"Please enter the size:";
   cin>>size;
   Circular c;
+  c.Q(size);
+  
+  c.Enqueue(20);
+  c.Enqueue(30);
+  c.Enqueue(50);
 
+  c.Display();
+  cout<<"Dequeues Value is"<<c.Dequeue();
+  
   return 0;
 }
