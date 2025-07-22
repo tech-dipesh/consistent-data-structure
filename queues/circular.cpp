fe8ad8c747efa949e065c6f0c8a3f71679ee01e4
Dipesh <dipeshsharmagautam@outlook.com>
2025-07-22 17:35:24 +0530

    final fix of the ciruclar queues

--- queues/circular.cpp
+++ queues/circular.cpp
@@ -14,13 +14,18 @@ class Circular{
   int rear;
   int *Q;
   public:
-  Circular(int sz):(sz), front(-1), rear(-1){int Q[size];}
+  // Circular(int sz):(sz), front(-1), rear(-1){int Q[size];}
+  // the front and rear should start from 0 not -1, also on the sz i should define a size.;
+  Circular(int sz):size(sz), front(0), rear(0){Q= new int[size];}
   ~Circular(){delete [] Q;}
   void Enqueue(int x){
-    if(isFull()) cout<<"Enqueue is Full";
+    if(isFull()) {
+      cout<<"Enqueue is Full:"<<endl;
+      return;
+    }
     else{
       // Q[size]=;
-      rear=(rear+1)%q;
+      rear=(rear+1)%size;
       Q[rear]=x;
     }
   }
@@ -34,35 +39,46 @@ class Circular{
     return x;
   }
 
-  void isFull(){
-    if(rear+1==size)cout<<"Queue is full";
+  bool isFull(){
+    // if(rear+1==size)cout<<"Queue is full";
+    return (rear+1)%size==front;
   }
-  void isEmpty(){
-    if(front==rear)cout<<"Queue is Empty";
+  bool isEmpty(){
+    // if(front==rear)cout<<"Queue is Empty";
+    return front==rear;
   }
 
   void Display(){
+    if(isEmpty()){
+      cout<<"Queue is empty";
+    }
     int i=front+1;
-    for(int i=(i+1)%size;i!=(rear+1)%size;i++){
-      cout<<Q[i];
+    // for(int i=(i+1)%size;i!=(rear+1)%size;i++){
+    while(true){
+      cout<<Q[i]<<" ";
+      // for breaking a condition.
+      if(i==rear) break;
+      i=(i+1)%size;
     }
     cout<<endl;
   }
 };
 
 int main(){
-  int size;
-  cout<<"Please enter the size:";
-  cin>>size;
-  Circular c;
-  c.Q(size);
+  // int size;
+  // cout<<"Please enter the size:";
+  // cin>>size;
+  // Circular c;
+  // Circular c(size);
+  Circular c(3);
+  // c.Q(size);
   
   c.Enqueue(20);
   c.Enqueue(30);
   c.Enqueue(50);
 
   c.Display();
-  cout<<"Dequeues Value is"<<c.Dequeue();
+  cout<<"Dequeues Value is: "<<c.Dequeue();
   
   return 0;
 }
