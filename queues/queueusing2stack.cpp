8bf224093ed8095f93ba98b2dec7b1f912f131e1
Dipesh <dipeshsharmagautam@outlook.com>
2025-07-24 14:45:19 +0530

    starting file of 2 stack

--- queues/queueusing2stack.cpp
+++ queues/queueusing2stack.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 #include <stack>
+class Stack{
+  private:
+  int size;
+  int front;
+  int rear;
+  int *Q;
+  public:
+  Stack(int size=10):front(-1), rear(-1){
+      Q=new int[size];
+  }
+  ~Stack(){
+    delete [] Q;
+  }
+  
+};
+
+
+int main(){
+  
+  return 0;
+}
