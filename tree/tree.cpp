e1614987d85ed3cf18df9dee39c29624dd6b9b77
Dipesh <dipeshsharmagautam@outlook.com>
2025-07-25 15:52:42 +0530

    start a tree.cpp file with boilerplate code

---
+++ tree/tree.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "intro.cpp"
+using namespace std;
+class Tree{
+  public:
+  Node *root;
+  Tree(){root=nullptr;}
+
+  void createTree(Node *p){
+      Node *p, *t;
+      int x;
+      Queue q;
+      
+  }
+
+  void PreOrder(Node *p){}
+  void PostOrder(Node *p){}
+  void InOrder(Node *p){}
+  void LevelOrder(Node *p){}
+  void Height(Node *root){}
+
+};
+int main(){
+    
+return 0;
+}
