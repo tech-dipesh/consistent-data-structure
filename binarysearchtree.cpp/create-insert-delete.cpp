5e899e796971b31c826c08915ef8526fcfa66043
Dipesh <dipeshsharmagautam@outlook.com>
2025-08-01 10:53:02 +0530

    write the initial logic of the binary search tree

--- binarysearchtree.cpp/create-insert-delete.cpp
+++ binarysearchtree.cpp/create-insert-delete.cpp
@@ -8,14 +8,62 @@ class BinaryTree{
       Node *rchild;
   }
   public:
-  Node *root;
+  Node *root=nullptr;
   void Insert(int val){
+      Node *t=root;
+      Node *r, *p;
+      if(root==nullptr){
+        p=new Node();
+        p->data=key;
+        p->lchild=p->rchild=nullptr;
+        return;
+      }
+      while(t){
+        r=t;
+        if(val<t->data)
+          t=t->lchild;
+          else if(key>t->data)
+            t=t->rchild;
+            else return;
+      }
+      p=new Node();
+      p->data=val;
+      p->lchild=p->rchild=nullptr;
 
+      if(key<r->data) r->lchild=p;
+      else r->rchild=;
   }
+
+  void Inorder(Node *p){
+    if(p)
+      Inorder(p->lchild);
+      cout<<p->data;
+      Inorder(p->rchild);
+  }
+  void Preorder(Node *p){
+    if(p)
+    cout<<p->data;
+      Inorder(p->lchild);
+      Inorder(p->rchild);
+  }
+  void Postorder(Node *p){
+    if(p)
+    Inorder(p->lchild);
+    Inorder(p->rchild);
+    cout<<p->data;
+  }
+
+  
+    // thisi is the function to access private dat to main without exposing a data:
+  Node *getRoot(){return root;}
 };
 int main(){
   BinaryTree b;
   b.Insert(10);
-  b.Insert(20);
+  b.Insert(30);
+  b.Insert(35);
+  b.Insert(40);
+
+  b.Inorder(b.getRoot());
 return 0;
 }
