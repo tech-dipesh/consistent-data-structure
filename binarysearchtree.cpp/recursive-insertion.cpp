ef592326eb72a0bbe1baf153aa8c236b1e71198e
Dipesh <dipeshsharmagautam@outlook.com>
2025-08-02 11:19:44 +0530

    update the recursive insertion code

---
+++ binarysearchtree.cpp/recursive-insertion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class Recursive{
+  private:
+  struct Node
+  {
+    Node *lchilc;
+    int data;
+    Node *rchild;
+  };
+  public:
+  Node *root=nullptr;
+  
+  
+};  
+int main(){
+   Recursive r;
+
+return 0;
+}
