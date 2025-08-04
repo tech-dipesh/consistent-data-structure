770ecf437b63fb935326d59f5c38726e5da0fd6f
Dipesh <dipeshsharmagautam@outlook.com>
2025-08-04 23:55:02 +0530

    write all major ll rotation code

--- avl-tree/ll-rotation.cpp
+++ avl-tree/ll-rotation.cpp
@@ -1,16 +1,87 @@
 #include <iostream>
 using namespace std;
-class LLRotation{
+class LLR{
   private:
   struct Node{
-
+      Node *lchild;
+      int data;
+      int height;
+      Node *rchild;
   };
   public:
+  Node *root=nullptr;
 
+  // Node *NodeHeight(Node *p){
+  int NodeHeight(Node *p){
+    int heightleft, heightright;
+    // For handling the edge case:
+    heightleft=p && p->lchild ? p->lchild->height:0;
+    heightright=p && p->rchild?p->rchild->height:0;
+
+    return heightleft>heightright?heightleft+1:heightright+1;
+  }
+
+  int BalancedFactor(Node *p){
+      int heightleft, heightright;
+    // For handling the edge case:
+    heightleft=p && p->lchild ? p->lchild->height:0;
+    heightright=p && p->rchild?p->rchild->height:0;
+    return heightleft-heightright;
+  }
+
+  Node *LLRotation(Node *p){
+    Node *pl=p->lchild;
+    Node *plr=pl->rchild;
+
+    pl->rchild=p;
+    p->lchild=plr;
+    // for updating the heights
+    p->height=NodeHeight(p);
+    pl->height=NodeHeight(pl);
+
+    if(root==p)
+      root=pl;
+    return pl;
+  }
+  
+  Node *RightInsert(Node *p, int key){
+    if(p==nullptr){
+     Node *t=new Node();
+      t->data=key;
+      t->height=1;
+      t->lchild=t->rchild=nullptr;
+      return t;
+    }
+
+    if(key<p->data)
+     p->lchild=RightInsert(p->lchild, key);
+    else if(key>p->data)
+      p->rchild=RightInsert(p->rchild, key);
+    // p->height=1;
+    p->height=NodeHeight(p);
+
+    // if that the heightfacctor is absolute value:
+    if(BalancedFactor(p)==2 && BalancedFactor(p->lchild)==1)
+        return LLRotation(p);
+    if(BalancedFactor(p)==2 && BalancedFactor(p->lchild)==-1)
+        return LLRotation(p);
+    if(BalancedFactor(p)==-2 && BalancedFactor(p->lchild)==-1)
+        return LLRotation(p);
+    if(BalancedFactor(p)==-2 && BalancedFactor(p->lchild)==1)
+        return LLRotation(p);
+
+    return p;
+  }
+
+
+  // getting the private value:
+  Node *Getter(){return root;}
 };
 
 int main(){
-    LLRotation ll;
-    
+   LLR ll;
+  ll.root=ll.RightInsert(ll.Getter(), 26);  
+  ll.root=ll.RightInsert(ll.Getter(), 13);
+  ll.root=ll.RightInsert(ll.Getter(), 6);
 return 0;
 }
