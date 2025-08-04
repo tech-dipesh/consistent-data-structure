47f8b23d40fc6fde0a7449d0936c7d0185508d2d
Dipesh <dipeshsharmagautam@outlook.com>
2025-08-04 10:37:55 +0530

    update the intro file of avl tree

--- avl-tree/intro.cpp
+++ avl-tree/intro.cpp
@@ -9,10 +9,67 @@ class AVLTree{
     Node *rchild;
   };
   public:
-  Node *
+  Node *root=nullptr;
+  void Insert(int value){
+    Node *t=root;
+    Node *r, *p;
+    if(root==nullptr){
+      p=new Node();
+      p->data=value;
+      p->lchild=p->rchild=nullptr;
+      root=p;
+      return;
+    }
+    while(t){
+      r=t;
+      if(value<t->data) t=t->lchild;
+      else if(value>t->data)t=t->rchild;
+      else return;
+    }
+
+    p=new Node();
+    p->data=value;
+    p->lchild=p->rchild=nullptr;
+
+    if(value<r->data)r->lchild=p;
+    else r->rchild=p;
+  }
+  
+  void Inorder(Node *p){
+    if(p){
+      Inorder(p->lchild);
+      cout<<p->data;
+      Inorder(p->rchild);
+    }
+  }
+
+  Node *Search(int value){
+      Node *t=root;
+      while(t){
+        if(value==t->data)
+          return t;
+          else if(value<t->data)
+            t=t->lchild;
+            else t=t->rchild;
+      }
+      return nullptr;
+  }
+  // Get root value outside of class:
+  Node *GetPrivate(){Node *root;}
 };
+
 int main(){
-    
-cout<<
+  AVLTree avl;
+  avl.Insert(10);
+  avl.Insert(3);
+  avl.Insert(25);
+  avl.Insert(32);
+  // inserting with inorder:
+  avl.Inorder(avl.GetPrivate());
+  cout<<endl;
+  // Node *temp;
+  // temp=avl.Search(20);
+  if(avl.Search(30)) cout<<"Element is found"<<avl.Search(30);
+  else cout<<"Element is not found";
 return 0;
 }
