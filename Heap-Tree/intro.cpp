dd21c3433316b5b110834d68a799a173bdca293d
Dipesh <dipeshsharmagautam@outlook.com>
2025-08-08 22:42:50 +0530

    update the intro code

--- Heap-Tree/intro.cpp
+++ Heap-Tree/intro.cpp
@@ -9,11 +9,34 @@ void Insert(int Heap[], int n){
   }
   Heap[i]=temp;
 }
+int Delete(int Heap[], int n){
+  int x=Heap[n];
+  Heap[1]=Heap[n];
+  int i=1;
+  int j=i*2;
+  while(j<n-1){
+    if(Heap[j+1]>Heap[j])
+        j+=1;
+        if(Heap[i]<Heap[j]){
+          temp=Heap[i];
+        Heap[i]=Heap[j];
+        Heap[j]=temp;      
+        // moving to child pointer:
+        i=j;
+        j=2*j;
+       }
+       else break;
+  }
+}
+
 int main(){
-   int Heap[]={5, 8, 13, 5, 7, 9, 4}; 
-   Insert(Heap, 2);
-   Insert(Heap, 3);
-   Insert(Heap, 4);
-   Insert(Heap, 5);
+   int Heap[]={5, 8, 13, 5, 7, 9, 4, 19, 20}; 
+  //  insertion with for loop
+  for(int i=2;i<=8;i++)
+    Insert(Heap, i); 
+
+    for(int i=1;i<=8;i++){
+      cout<<Heap[i]<<" ";
+    }
 return 0;
 }
