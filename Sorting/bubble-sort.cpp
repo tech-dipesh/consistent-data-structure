1df7285dda11f213e521355d96c47ab512d8aa64
Dipesh <dipeshsharmagautam@outlook.com>
2025-08-10 00:09:10 +0530

    write the base logic with have the error which i've fix later.

--- Sorting/bubble-sort.cpp
+++ Sorting/bubble-sort.cpp
@@ -4,18 +4,42 @@ class BinaryTree{
    public:
    void Sorting(int A[], int n){
          for(int i=0;i<n;i++){
-            for(int j=i+1;j<n-i-1;j++){
-               if(A[])
+            for(int j=0;j<n-i-1;j++){
+               if(A[j]>A[j+1])
+                  swap(A[j], A[j+1]);
             }
          }
 
-   }
-   int Display(){
 
-   }
+         void isSorted(int A[], int n){
+            int checkingSortation;
+             for(int i=0;i<n;i++){
+               checkingSortation=0;
+            for(int j=0;j<n-i-1;j++){
+               if(A[j]>A[j+1]){
+                  swap(A[j], A[j+1]);
+                  checkingSortation=1;
+            }
+         }
+         if(checkingSortation====0){
+            cout<<"Bubble sort is already sorted no nneed more checking";
+            break;
+      }
+             }
+         }
+
+
+   };
 };
 int main(){
  BinaryTree b;
+   int A[]={5, 19, 12, 9, 14};
+   int n=5;
+   b.Sorting(A[], n);
+   b.isSorted(A[], n);
 
+   // printing the sorted algorithm:
+   for(int i=0;i<n;i++)
+      cout<<A[i];
 return 0;
 }
