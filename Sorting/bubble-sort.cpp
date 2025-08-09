a5b6bbed9b22d30202e5d85861b1567620f7c9da
Dipesh <dipeshsharmagautam@outlook.com>
2025-08-09 22:49:43 +0530

    update the bubble sort

--- Sorting/bubble-sort.cpp
+++ Sorting/bubble-sort.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
+class BinaryTree{
+   public:
+   void Sorting(int A[], int n){
+         for(int i=0;i<n;i++){
+            for(int j=i+1;j<n-i-1;j++){
+               if(A[])
+            }
+         }
+
+   }
+   int Display(){
+
+   }
+};
 int main(){
-    
+ BinaryTree b;
+
 return 0;
 }
