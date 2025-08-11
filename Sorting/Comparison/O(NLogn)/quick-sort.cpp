b0b627b529a4b4277f5e88aacb2f11adfbc5d51f
Dipesh <dipeshsharmagautam@outlook.com>
2025-08-11 23:01:57 +0530

    move logn to nlogn and also insert the array

---
+++ Sorting/Comparison/O(NLogn)/quick-sort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class QuickSort{
+  public:
+  int QuickSorting(int Arr[], int l, int h){
+
+  }
+};
+int main(){
+    int S[]={31, 82, 29, 24, 11, 42, 61, 92, 6, 52, 98, 68, 9, 13, 4, 15, 56, 64, 21, 35, 46, 74, 85, 37, 88};
+    
+   QuickSort q; 
+return 0;
+}
