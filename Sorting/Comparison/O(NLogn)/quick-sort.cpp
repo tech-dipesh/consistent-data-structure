b2c4e42ba2e681b78fa9be8d75f7cc32f68281eb
Dipesh <dipeshsharmagautam@outlook.com>
2025-08-12 13:01:12 +0530

    update the code

--- Sorting/Comparison/O(NLogn)/quick-sort.cpp
+++ Sorting/Comparison/O(NLogn)/quick-sort.cpp
@@ -5,7 +5,7 @@ class QuickSort{
   // youtube approach:
 //   int Parition(int Arr[], int low, int h){
 // int pivot=Arr[low];
-// int i=Arr[low], j=Arr[h];
+// int i=low, j=high;
 // while(i<j){
 //   while(Arr[i]<=Arr[pivot] && i<=h){
 //       i++;
@@ -13,7 +13,7 @@ class QuickSort{
 //   while(Arr[j]>Arr[pivot] && j>=low){
 //       j--;
 //   }
-//   if(i<j)swap(Arr[i], Arr[j]);
+//   if(i<j)swap(Arr[low], Arr[j]);
 // }
 //   }
 
@@ -21,24 +21,24 @@ class QuickSort{
   // abdul bari approach:
    int Parition(int Arr[], int low, int high){
 int pivot=Arr[low];
-int i=Arr[low], j=Arr[high];
+// int i=Arr[low], j=Arr[high];
+int i=low, j=high;
 // while(i<j){
 do{
   do {i++;} while (Arr[i]<=pivot);
   do {j--;} while (Arr[j]>pivot);
   if(i<j)swap(Arr[i], Arr[j]);
 } while(i<j);
-swap(Arr[low], Arr[high]);
+swap(Arr[low], Arr[j]);
 return j;
    }
 
   //  Recusrive approch:
   void quickVoidSort(int A[], int low, int high){
-    int j;
     if(low<high){
-      j=Parition(A, low, high);
-      QuickSort(A, low, j);
-      QuickSort(A, j+1, high);
+     int j=Parition(A, low, high);
+      quickVoidSort(A, low, j);
+      quickVoidSort(A, j+1, high);
     }
   }
 
@@ -54,9 +54,11 @@ int main(){
     int S[]={31, 82, 29, 24, 11, 42, 61, 92, 6, 52, 98, 68, 9, 13, 4, 15, 56, 64, 21, 35, 46, 74, 85, 37, 88};
     int size=24;
     int low=0, high=size;
+    cout<<endl<<"before partition:";
+    q.Display(S, size);
     q.Parition(S, low, high);
     q.quickVoidSort(S, low, high);
     cout<<endl<<"After printing:";
-    q.Display(S, low, high);
+    q.Display(S, size);
 return 0;
 }
