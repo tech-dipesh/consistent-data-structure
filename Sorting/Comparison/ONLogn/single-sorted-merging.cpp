514dcab57c8cb2c62a3e50fae8ca8abb6c10ad18
Dipesh <dipeshsharmagautam@outlook.com>
2025-08-15 22:28:54 +0530

    update the sorted merged lsit

---
+++ Sorting/Comparison/ONLogn/single-sorted-merging.cpp
@@ -0,0 +1,21 @@
+// i have to also check multipe elementleft might over after looping it..
+
+class Single{
+  public:
+  void Merge(int A[], int asize, int C[], int csize){
+    int i=j=k=0;
+    while(i<asize && j<bsize){
+      if()
+    }
+  }
+};
+#include <iostream>
+using namespace std;
+int main(){
+  Single s;
+  int a={21, 24, 29, 32, 40, 19, 27, 34, 45}
+  int asize=10;
+  int *C=new int[asize];
+  s.Merge(A, asize, C, asize);
+return 0;
+}
